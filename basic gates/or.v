699175bd7776a3c6a9128bc27883cdf193d4463e
toshanichetna07 <toshanichetna07@gmail.com>
2025-05-27 16:06:40 +0530

    Update or.v

--- basic gates/or.v
+++ basic gates/or.v
@@ -1,3 +1,24 @@
 module(output y,input a, b);
   assign y = a|b;
 endmodule
+
+
+//testbench:
+module or_gate_tb;
+
+  // Declare testbench variables
+  reg a, b;
+  wire out;
+
+  // Instantiate the OR gate primitive
+  or_gate uut (out, a, b); // uut = Unit Under Test
+
+  // Apply test vectors
+  initial begin
+    $display("Time | a b | out");
+    $monitor("%4t | %b %b |  %b", $time, a, b, out);
+
+    // Test all input combinations
+    a = 0; b = 0; #10;
+    a = 0; b = 1; #10;
+    a = 1; b
