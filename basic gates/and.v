3ad144d47cceee89df194c400cd4de075f3e4f9a
toshanichetna07 <toshanichetna07@gmail.com>
2025-05-27 16:11:53 +0530

    Update and.v

--- basic gates/and.v
+++ basic gates/and.v
@@ -1,3 +1,26 @@
 module(output y,input a, b);
   assign y = a & b;
 endmodule
+
+testbench:
+module and_gate_tb;
+
+  reg a, b;
+  wire out;
+
+  // Instantiate the AND gate primitive
+  and_gate uut(out, a, b);
+
+  initial begin
+    $display("Time | a b | out");
+    $monitor("%4t | %b %b |  %b", $time, a, b, out);
+
+    a = 0; b = 0; #10;
+    a = 0; b = 1; #10;
+    a = 1; b = 0; #10;
+    a = 1; b = 1; #10;
+
+    $finish;
+  end
+
+endmodule
