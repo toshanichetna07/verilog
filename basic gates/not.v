018583e3b6aaf1add26fc7b5608cf9806c28eab3
toshanichetna07 <toshanichetna07@gmail.com>
2025-05-27 16:16:29 +0530

    Update not.v

--- basic gates/not.v
+++ basic gates/not.v
@@ -1,3 +1,25 @@
 module(output y, input a);
   assign y = ~a;
 endmodule
+
+//testbench:
+module not_gate_tb;
+
+  reg a;
+  wire out;
+
+  // Instantiate the NOT gate primitive
+  not_gate uut(out, a);
+
+  initial begin
+    $display("Time | a | out");
+    $monitor("%4t | %b |  %b", $time, a, out);
+
+    a = 0; #10;
+    a = 1; #10;
+
+    $finish;
+  end
+
+endmodule
+
